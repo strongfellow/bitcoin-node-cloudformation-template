1a47feef9199a9688b592001da80f1336b8f0415
strongfellow <a@b.com>
2016-03-05 07:24:04 -0800

    allow choice of template

--- start-node.sh
+++ start-node.sh
@@ -3,15 +3,18 @@
 set -e
 set -x
 
-STACK_NAME="btc-2016-03-04-v-02"
+STACK_NAME="btc-2016-03-05-v-02"
 VPC="vpc-599c723d"
 SUBNET="subnet-09ee1e6d"
 RPC_PASSWORD="ajfkldfdasf"
 KEY_NAME="bitcoin-nodes"
 INSTANCE_TYPE="c4.large"
 
+TEMPLATE=node.json
+# TEMPLATE=0mq-node.json
+
 aws cloudformation create-stack \
   --stack-name ${STACK_NAME} \
-  --template-body file://./0mq-node.json \
+  --template-body file://./${TEMPLATE} \
   --parameters ParameterKey=VPC,ParameterValue=${VPC} ParameterKey=Subnet,ParameterValue=${SUBNET} ParameterKey=BitcoinRpcPassword,ParameterValue=${RPC_PASSWORD} ParameterKey=KeyName,ParameterValue=${KEY_NAME} ParameterKey=InstanceType,ParameterValue=${INSTANCE_TYPE}
 
