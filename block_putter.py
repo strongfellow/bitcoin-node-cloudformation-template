41da261f6a02e8813d911a7676eec32b1ecfd179
strongfellow <a@b.com>
2016-03-09 09:01:00 -0800

    be smart, dont put if we already have it

--- block_putter.py
+++ block_putter.py
@@ -1,8 +1,11 @@
 
+import boto3
+import botocore
 import binascii
 import hashlib
 import itertools
 import sys
+import logging
 
 def bytes_from_input(input, chunksize=8192):
     while True:
@@ -34,10 +37,41 @@ def little_endian(bytes):
         i += 1
     return n
 
-bs = bytes_from_input(sys.stdin)
-while True:
-    network = hex(bytearray([bs.next(), bs.next(), bs.next(), bs.next()]))
-    length = little_endian(itertools.islice(bs, 4))
-    block = bytearray(itertools.islice(bs, length))
-    h = block_hash(block)
-    print '%s\t%d\t%s' % (network, length, h)
+def main():
+    logging.basicConfig(level=logging.INFO)
+    bs = bytes_from_input(sys.stdin)
+    s3 = boto3.client('s3')
+    BUCKET = 'strongfellow.com'
+    while True:
+        network = hex(bytearray([bs.next(), bs.next(), bs.next(), bs.next()]))
+        length = little_endian(itertools.islice(bs, 4))
+        block = bytearray(itertools.islice(bs, length))
+        h = block_hash(block)
+
+        md5 = hex(hashlib.md5(block).digest())
+        print '%s\t%d\t%s\t%s' % (network, length, h, md5)
+        key = 'blocks/%s' % h
+        try:
+            response = s3.head_object(
+                Bucket=BUCKET,
+                Key=key,
+                IfMatch=md5)
+            logging.info('cache hit for %s' % key)
+
+        except botocore.exceptions.ClientError as e:
+            # If a client error is thrown, then check that it was a 404 error.
+            # If it was a 404 error, then the bucket does not exist.
+            error_code = int(e.response['Error']['Code'])
+            if error_code == 404 or error_code == 412:
+                logging.info('expected exception, object not found or md5 didn\'t match')
+                logging.info('putting %s', key)
+                s3.put_object(
+                    Bucket=BUCKET,
+                    Key=key,
+                    Body=bytes(block))
+            else:
+                logging.exception('unknown exception')
+
+if __name__ == '__main__':
+    main()
+
