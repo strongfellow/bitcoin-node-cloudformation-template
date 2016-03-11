90ca3632bfb37e6219933fef931ad272afb4eeaf
strongfellow <a@b.com>
2016-03-10 21:43:14 -0800

    simplify to just deal with byte strings

--- block_putter.py
+++ block_putter.py
@@ -7,14 +7,11 @@ import itertools
 import sys
 import logging
 
-def bytes_from_input(input, chunksize=8192):
-    while True:
-        chunk = input.read(chunksize)
-        if chunk:
-            for b in chunk:
-                yield b
-        else:
-            break
+def read_exactly(input, n):
+    result = input.read(n)
+    if len(result) != n:
+        raise EOFError
+    return result
 
 def hex(bs):
     return binascii.hexlify(bs)
@@ -29,49 +26,42 @@ def block_hash(bs):
 def tx_hash(bs):
     return double_sha256(bs)
 
-def little_endian(bytes):
-    i = 0
-    n = 0
-    for b in (ord(x) for x in bytes):
-        n = n + (b << (i * 8))
-        i += 1
-    return n
+def little_endian(input, n):
+    bs = read_exactly(input, n)
+    return sum(ord(b) << (i * 8) for i, b in enumerate(bs)) 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    bs = bytes_from_input(sys.stdin)
     s3 = boto3.client('s3')
     BUCKET = 'strongfellow.com'
     while True:
-        network = hex(bytearray([bs.next(), bs.next(), bs.next(), bs.next()]))
-        length = little_endian(itertools.islice(bs, 4))
-        block = bytearray(itertools.islice(bs, length))
+        network = hex(read_exactly(sys.stdin, 4))
+        length = little_endian(sys.stdin, 4)
+        block = read_exactly(sys.stdin, length)
         h = block_hash(block)
+
+        if network != 'f9beb4d9':
+            raise Exception('unexpected network: %s' % network)
 
         md5 = hex(hashlib.md5(block).digest())
         print '%s\t%d\t%s\t%s' % (network, length, h, md5)
         key = 'blocks/%s' % h
         try:
-            response = s3.head_object(
-                Bucket=BUCKET,
-                Key=key,
-                IfMatch=md5)
+            response = s3.head_object(Bucket=BUCKET, Key=key, IfMatch=md5)
             logging.info('cache hit for %s' % key)
-
         except botocore.exceptions.ClientError as e:
-            # If a client error is thrown, then check that it was a 404 error.
-            # If it was a 404 error, then the bucket does not exist.
             error_code = int(e.response['Error']['Code'])
             if error_code == 404 or error_code == 412:
-                logging.info('expected exception, object not found or md5 didn\'t match')
+                if error_code == 404:
+                    logging.info('not found')
+                if error_code == 412:
+                    logging.info('found but md5 mismatch')
                 logging.info('putting %s', key)
-                s3.put_object(
-                    Bucket=BUCKET,
-                    Key=key,
-                    Body=bytes(block))
+                s3.put_object(Bucket=BUCKET, Key=key, Body=block)
+                logging.info('SUCCESS putting %s', key)
             else:
                 logging.exception('unknown exception')
+                raise
 
 if __name__ == '__main__':
     main()
-
