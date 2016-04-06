f949b5831fade02c122eb557751ac21faabfd4bf
strongfellow <a@b.com>
2016-04-05 21:36:26 -0700

    0mq template and regular template working nicely

--- strongfellowbtc/tests/test_double_sha256.py
+++
@@ -1,9 +0,0 @@
-
-import strongfellowbtc
-
-def test():
-    actual = strongfellowbtc.double_sha256('hello')
-    print actual
-    expected = '9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50'.decode('hex')[::-1].encode('hex')
-    print expected
-    assert actual == expected
