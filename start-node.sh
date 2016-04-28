d7ebd6322ed98516e7a849b137bc12f6470c7849
strongfellow <a@b.com>
2016-04-27 20:58:03 -0700

    update users

--- start-node.sh
+++ start-node.sh
@@ -3,13 +3,11 @@
 set -e
 set -x
 
-#NODE_TYPE="0mq-"
-#NODE_TYPE=""
-NODE_TYPE="bitcoin-install"
+NODE_TYPE="baked-node"
 
 TIMESTAMP=`date +%s`
 DATE=`date +%Y-%m-%d`
-STACK_NAME=btc-${NODE_TYPE}${DATE}-${TIMESTAMP}
+STACK_NAME=btc-${NODE_TYPE}-${DATE}-${TIMESTAMP}
 echo ${STACK_NAME}
 TEMPLATE=${NODE_TYPE}.json
 echo ${TEMPLATE}
@@ -17,10 +15,9 @@ echo ${TEMPLATE}
 VPC="vpc-599c723d"
 SUBNET="subnet-09ee1e6d"
 RPC_PASSWORD="ajfkldfdasf"
-KEY_NAME="bitcoin-nodes"
-INSTANCE_TYPE="c4.2xlarge"
-#INSTANCE_TYPE="m3.medium"
-#SNAPSHOT="snap-e9286cbb"
+KEY_NAME="thinkpad"
+INSTANCE_TYPE="m3.medium"
+SNAPSHOT="snap-c2367687"
 
 # DO_NOTHING | ROLLBACK | DELETE
 ON_FAILURE=DO_NOTHING
@@ -34,6 +31,5 @@ aws cloudformation create-stack \
                ParameterKey=Subnet,ParameterValue=${SUBNET} \
                ParameterKey=KeyName,ParameterValue=${KEY_NAME} \
                ParameterKey=InstanceType,ParameterValue=${INSTANCE_TYPE} \
-#               ParameterKey=BitcoinRpcPassword,ParameterValue=${RPC_PASSWORD} \
-#              ParameterKey=Snapshot,ParameterValue=${SNAPSHOT}
-
+               ParameterKey=BitcoinRpcPassword,ParameterValue=${RPC_PASSWORD} \
+              ParameterKey=Snapshot,ParameterValue=${SNAPSHOT}
